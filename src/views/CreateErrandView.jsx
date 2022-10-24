8bed916cac2da79c6ab9a5296dd5c20e43923477
CasperBjarnetoft <casper.ason@hotmail.se>
2022-10-24 13:59:58 +0200

    lagt till kod

--- src/views/CreateErrandView.jsx
+++ src/views/CreateErrandView.jsx
@@ -5,18 +5,13 @@ const CreateErrandView = () => {
     <div className='container mt-5 bg-light rounded'>
       <form className='p-4'>
         <div className="form mb-4">
-          <label className="form-label">User</label>
+          <label className="form-label">E-post</label>
           <input type="text" name="title" className="form-control" />
         </div>
 
         <div className="form mb-4">
           <label className="form-label">title</label>
           <input type="text" name="title" className="form-control" />
-        </div>
-
-        <div className="form datetimepicker mb-4">
-          <label className="form-label">Date and Time </label>
-          <input type="datetime-local" name="date" className="form-control" id="datetimepickerExample" />
         </div>
 
         <div className="form mb-4">
